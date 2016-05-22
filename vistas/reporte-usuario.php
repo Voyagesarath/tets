d15c75b72cb55ef054b415e28973fe527afecc79
oscarbroa <oscarbroa@gmail.com>
2016-05-21 20:34:11 -0700

    21/5/16 - 20:34

---
+++ vistas/reporte-usuario.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+		<?php
+session_start();
+if (@!$_SESSION['user']) {
+	header("Location:../index.php");
+}
+?>
+<html lang="en">
+  <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Editorial ITH</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<link rel="shortcut icon" href="../images/favicon.ico">
+<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+<link href="../bootstrap/css/bootstrap2.css" rel="stylesheet">
+<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="../bootstrap/css/bootstrap-theme2.css" rel="stylesheet">
+<link href="../bootstrap/css/bootstrap-theme2.min.css" rel="stylesheet">
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<script src="../bootstrap/js/bootstrap.js"></script>
+<script src="../js/jquery.js"></script>
+<script src="../js/myjava.js"></script>
+  </head>
+  <style type="text/css">
+body {
+	background-color: ##F2F2F2;
+	
+}
+body,td,th {
+	color: ##000000;
+}
+
+</style>
+<header>
+  <img src="../images/header-ith.png">
+
+</header>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="100%" border="0">
+  <tr>
+    <td>&nbsp;</td>
+    <!--<td width="96%" align="center"><img src="editorial.png" width="820" height="100" /></td>-->
+    <td>&nbsp;</td>
+  </tr>
+<div class="container">
+<header class="header">
+<div class="row">
+	</div>
+</header>
+
+  <!-- Navbar
+    ================================================== -->
+
+
+<div class="navbar">
+  <div class="navbar-inner">
+	<div class="container">
+	  <div class="nav-collapse">
+		<ul class="nav">
+			<li class=""><a href="principal.php">Principal</a></li>
+			 
+	
+		</ul>
+		<form action="#" class="navbar-search form-inline" style="margin-top:6px">
+		
+		</form>
+		<ul class="nav pull-right">
+		<li><a href="">Bienvenido <strong><?php echo $_SESSION['user'];?></strong> </a></li>
+			  <li><a href="../php/desconectar.php"> Cerrar Cesión </a></li>			 
+		</ul>
+	  </div><!-- /.nav-collapse -->
+	</div>
+  </div><!-- /navbar-inner -->
+</div>
+
+<!-- ======================================================================================================================== -->
+<div class="row">
+	
+	
+		
+	<div class="span12">
+
+		<div class="caption">
+		
+<!--///////////////////////////////////////////////////Empieza cuerpo del documento interno////////////////////////////////////////////-->
+		<h2> Reporte de Servicios Por Usuarios</h2>	
+		<div class="well well-small">
+		<hr class="soft"/>
+		<h4>Tabla de Usuarios</h4>
+		<div class="row-fluid">
+
+
+
+      	<?php
+				require("../php/connect_db.php");
+$registro = ("SELECT * FROM reg_serv_copiado where maestro = $id order by id_reg_serv_copiado ASC");
+		echo '<table class="table table-striped table-condensed table-hover">
+        	<tr>
+            	<th width="30">ID Servicio</th>
+                <th width="50">Departamento</th>
+                <th width="150">Maestro</th>
+                <th width="150">Numero de Copias</th>
+                <th width="150">Clave</th>
+                <th width="50">Fecha</th>
+            </tr>';
+            $query = mysql_query($registro) or die(mysql_error());
+if(mysql_num_rows($query)>0){
+	while($registro2 = mysql_fetch_array($query)){
+		echo '<tr>
+				<td>'.$registro2['id_reg_serv_copiado'].'</td>
+				<td>'.$registro2['departamento'].'</td>
+				<td>'.$registro2['maestro'].'</td>
+				<td>'.$registro2['num_copias'].'</td>
+				<td>'.$registro2['clave'].'</td>
+				<td>'.fechaNormal($registro2['fecha']).'</td>
+				</tr>';
+	}
+}else{
+	echo '<tr>
+				<td colspan="6">No se encontraron resultados</td>
+			</tr>';
+}
+echo '</table>';
+				
+			?>
+
+
+
+
+          
+				  
+			  			  
+			  
+		
+		
+		<div class="span8">
+		
+		</div>	
+		</div>	
+		<br/>
+		
+
+
+		<!--EMPIEZA DESLIZABLE-->
+		
+		 <!--TERMINA DESLIZABLE-->
+
+
+
+		
+		
+		</div>
+
+		
+
+
+		
+
+<!--///////////////////////////////////////////////////Termina cuerpo del documento interno////////////////////////////////////////////-->
+</div>
+
+	</div>
+</div>
+<!-- Footer
+      ================================================== -->
+<hr class="soften"/>
+
+</div><!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/js/jquery-1.8.3.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+	</style>
+  </body>
+</html>
