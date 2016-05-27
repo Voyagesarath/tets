e181a79b2ffc754fe367adafa5179e530397dffa
oscarbroa <oscarbroa@gmail.com>
2016-05-26 20:22:10 -0700

    0

--- php/connect_db.php
+++ php/connect_db.php
@@ -3,6 +3,12 @@
 $conexion = mysql_connect('localhost','root','');
 mysql_select_db('academ', $conexion);
 
+$user="root";
+$pass="";
+$server="localhost";
+$bd="academ";
+$con=mysqli_connect($host,$user,$pass,$bd);
+
 function fechaNormal($fecha){
 		$nfecha = date('d/m/Y',strtotime($fecha));
 		return $nfecha;
